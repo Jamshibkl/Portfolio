397a21fa4166b20d804e06c8b1673224db4a00f7
jamsheer <jamshibkl3@gmail.com>
2024-01-07 18:15:13 +0530

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/myRESUME.pdf";
+import pdf from "../../Assets/../Assets/Jamsheer.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
